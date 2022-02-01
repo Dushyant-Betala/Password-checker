e984d6985f9bbe4ded6ab46fb33fed28a724b5b3
Dushyant Betala <83596147+Dushyant-Betala@users.noreply.github.com>
2022-02-01 17:45:55 +0530

    Update checkmypass.py

--- checkmypass.py
+++ checkmypass.py
@@ -11,6 +11,7 @@ def request_api_data(query_char):
         raise RuntimeError(f'Error Fetching: {res.status_code}, check the api and try again')
     return res
 
+#This fuction takes input from the api by giving the first 5 character and checks the list with the enter password to make sure that the password matched the one from the list 
 def get_password_leaks_count (hashes, hash_to_check):
     hashes = (line.split(':') for line in hashes.text.splitlines())
     for h, count in hashes:
